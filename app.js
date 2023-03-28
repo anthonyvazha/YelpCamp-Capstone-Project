128054183e82b5102342a721f97aec64fe57fe3c
Anthony Vazhapilly <anthonyvazha@gmail.com>
2023-03-28 15:42:19 -0500

    Reviews Feature Complete - Model, Form, Validate, Display, Styling, Deleting, Middleware.

--- app.js
+++ app.js
@@ -102,6 +102,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
 
 
 
