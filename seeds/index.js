d819a0115a2c36200c3201fb997af777083da55a
Anthony Vazhapilly <anthonyvazha@gmail.com>
2023-04-04 14:26:12 -0500

    Added an Author to Campground Model

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000); 
         const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
+            author: '642c76cd02ac802773b5729c',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://source.unsplash.com/collection/483251`,
