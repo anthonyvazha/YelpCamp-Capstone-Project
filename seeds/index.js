bde3ce4df9fb60613d51074a23c8efc0b095bfce
Anthony Vazhapilly <anthonyvazha@gmail.com>
2023-04-11 20:59:09 -0500

    Completed Image Upload Feature

--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,39 @@ const seedDB = async () => {
             author: '642c76cd02ac802773b5729c',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://source.unsplash.com/collection/483251`,
+            // image: `https://source.unsplash.com/collection/483251`,
             description:"Loren ipsum - this is cool disctionption ifn you didn tha vhe have anthing",
-            price 
+            price,
+            images:  [
+                {
+                  url: 'https://res.cloudinary.com/dybznrhqn/image/upload/v1681183445/YelpCamp/pclytikumuvahuu01d8s.jpg',
+                  filename: 'YelpCamp/pclytikumuvahuu01d8s'
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dybznrhqn/image/upload/v1681183452/YelpCamp/f47vc4at2dwxkcgfaxgy.jpg',
+                  filename: 'YelpCamp/f47vc4at2dwxkcgfaxgy'
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dybznrhqn/image/upload/v1681183457/YelpCamp/sg9r4dnvebu7khu16rje.jpg',
+                  filename: 'YelpCamp/sg9r4dnvebu7khu16rje'
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dybznrhqn/image/upload/v1681183464/YelpCamp/pwyq1eho1t5opkmlbu02.jpg',
+                  filename: 'YelpCamp/pwyq1eho1t5opkmlbu02'
+                  
+                },
+                {
+                    url: 'https://res.cloudinary.com/dybznrhqn/image/upload/v1681183525/YelpCamp/wvovrl9lt3keevyc61ew.jpg',
+                    filename: 'YelpCamp/wvovrl9lt3keevyc61ew'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dybznrhqn/image/upload/v1681183532/YelpCamp/pke7fwr8hrfsbetw0lax.jpg',
+                    filename: 'YelpCamp/pke7fwr8hrfsbetw0lax'
+                }
+              ]
 
         })
         await camp.save();
